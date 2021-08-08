e3f0fc7423f05d45af7fe234c54fb9cddba12921
jo <ljonas@riseup.net>
2021-08-08 16:32:45 +0200

    Fix tests

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -2,13 +2,13 @@ from gh_release_install.utils import Log
 
 
 def test_log(capsys):
-    Log.level = 2
+    Log.set_level(1)
 
     Log.info("hello")
     Log.error("hello")
     Log.debug("hello")
 
-    Log.level = 3
+    Log.set_level(2)
     Log.debug("nope")
 
     captured = capsys.readouterr()
