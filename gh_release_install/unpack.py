19ddc3ca05d3c868db6dd0259651ffea9d7bf074
jo <ljonas@riseup.net>
2021-08-24 04:11:35 +0200

    Add support for bz2 compressed files

---
+++ gh_release_install/unpack.py
@@ -0,0 +1,27 @@
+import bz2
+from pathlib import Path
+from shutil import get_unpack_formats, register_unpack_format
+
+from .utils import Log
+
+
+def _unpack_bz2(filename, extract_dir):
+    filename = Path(filename)
+    extract_dir = Path(extract_dir)
+
+    extracted = extract_dir / filename.stem
+
+    with filename.open("rb") as filename_fd:
+        with extracted.open("wb") as extracted_fd:
+            extracted_fd.write(bz2.decompress(filename_fd.read()))
+
+
+def register_unpack_formats():
+    """Register custom unpack formats."""
+    Log.debug("Registering custom unpack formats.")
+
+    formats = get_unpack_formats()
+    if "bz2" not in map(lambda x: x[0], formats):
+        register_unpack_format("bz2", [".bz2"], _unpack_bz2, description="bz2 files")
+
+    Log.debug(f"Unpack formats available: {formats}")
