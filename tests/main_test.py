bef3b672319a495cd24bf4e64ae0cb223639501e
jo <ljonas@riseup.net>
2022-10-01 19:21:55 +0200

    refactor: simplify GhReleaseInstall

--- tests/main_test.py
+++ tests/main_test.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import json
 from pathlib import Path
 
-from gh_release_install.main import template_property
+from gh_release_install import GhReleaseInstall
 
 
 def _load_json_fixture(path: str) -> dict:
@@ -13,61 +13,55 @@ def _load_json_fixture(path: str) -> dict:
     return json.loads(raw)
 
 
-def test_template_property():
-    # pylint: disable=too-few-public-methods
-    class Some:
-        _tmpls = {}
-        name = "Charles"
-
-        @template_property
-        def greet(self):
-            return self._tmpls["greet"].format(name=self.name)
-
-    some = Some()
-    some.greet = "{name} World!"
-
-    assert some.greet == "Charles World!"
-    some.name = "Hello"
-    assert some.greet == "Hello World!"
-
-
-def test_installer_get_target_version_latest(requests_mock, installer):
+def test_installer_get_target_version_latest(
+    requests_mock,
+    installer: GhReleaseInstall,
+):
     requests_mock.get(
         "https://api.github.com/repos/prometheus/prometheus/releases/latest",
         json=_load_json_fixture("tests/fixtures/gh_releases_latest.json"),
     )
     installer._get_target_version()
 
-    assert installer._target_tag == "v2.28.1"
-    assert installer._target_version == "2.28.1"
+    assert installer._target is not None
+    assert installer._target.tag == "v2.28.1"
+    assert installer._target.version == "2.28.1"
 
 
-def test_installer_get_target_version_fixed(installer):
-    installer.version = "v2.28.1"
+def test_installer_get_target_version_fixed(installer: GhReleaseInstall):
+    installer._version = "v2.28.1"
     installer._get_target_version()
 
-    assert installer._target_tag == "v2.28.1"
-    assert installer._target_version == "2.28.1"
+    assert installer._target is not None
+    assert installer._target.tag == "v2.28.1"
+    assert installer._target.version == "2.28.1"
 
 
-def test_installer_get_local_version(installer, tmp_path):
-    installer.destination = tmp_path / "prometheus"
-    installer.version_file = "{destination}.version"
+def test_installer_get_local_version(
+    tmp_path: Path,
+    installer: GhReleaseInstall,
+):
+    installer._destination = str(tmp_path / "prometheus")
+    installer._version_file = "{destination}.version"
 
     installer._get_local_version()
 
-    assert installer._local_tag is None
-    assert installer._local_version is None
+    assert installer._local is None
 
 
-def test_installer_get_local_version_exists(installer, tmp_path):
-    installer.destination = tmp_path / "prometheus"
-    installer.version_file = "{destination}.version"
+def test_installer_get_local_version_exists(
+    tmp_path: Path,
+    installer: GhReleaseInstall,
+):
+    installer._destination = str(tmp_path / "prometheus")
+    installer._version_file = "{destination}.version"
 
     tmp_version_file = installer.version_file
+    assert tmp_version_file is not None
     tmp_version_file.write_text("v2.28.1")
 
     installer._get_local_version()
 
-    assert installer._local_tag == "v2.28.1"
-    assert installer._local_version == "2.28.1"
+    assert installer._local is not None
+    assert installer._local.tag == "v2.28.1"
+    assert installer._local.version == "2.28.1"
