0c882fe49472324c734d91d0c3971244583bbbfa
jo <ljonas@riseup.net>
2022-11-10 22:00:44 +0100

    test: print debug logs during examples run

--- examples.sh
+++ examples.sh
@@ -12,7 +12,7 @@ command -v gh-release-install > /dev/null || error "gh-release-install command n
 TMP_DIR=$(mktemp -d)
 pushd "$TMP_DIR"
 
-gh-release-install -v \
+gh-release-install -vv \
     'prometheus/node_exporter' \
     'node_exporter-{version}.linux-amd64.tar.gz' \
     --extract 'node_exporter-{version}.linux-amd64/node_exporter' \
@@ -21,27 +21,27 @@ gh-release-install -v \
     --version-file '{destination}.version' \
     --checksum 'sha256:sha256sums.txt'
 
-gh-release-install -v \
+gh-release-install -vv \
     'mvdan/sh' \
     'shfmt_{tag}_linux_amd64' \
     'shfmt' \
     --version-file '{destination}.version'
 
-gh-release-install -v \
+gh-release-install -vv \
     'mvdan/sh' \
     'shfmt_{tag}_linux_amd64' \
     'shfmt' \
     --version 'v3.3.1' \
     --version-file '{destination}.version'
 
-gh-release-install -v \
+gh-release-install -vv \
     'mvdan/sh' \
     'shfmt_{tag}_linux_amd64' \
     '.' \
     --version 'v3.3.1' \
     --version-file '{destination}.version'
 
-gh-release-install -v \
+gh-release-install -vv \
     'grafana/loki' \
     'loki-linux-amd64.zip' \
     --extract 'loki-linux-amd64' \
@@ -49,7 +49,7 @@ gh-release-install -v \
     --version 'v2.2.1' \
     --checksum 'sha256:SHA256SUMS'
 
-gh-release-install -v \
+gh-release-install -vv \
     'grafana/loki' \
     'loki-linux-amd64.zip' \
     --extract 'loki-linux-amd64' \
@@ -57,7 +57,7 @@ gh-release-install -v \
     --version 'v2.2.1' \
     --checksum 'sha256:dacfb229dbc7064b1d6390173ea6963eb3c85f60dc2336081b0113476405c5aa'
 
-gh-release-install -v \
+gh-release-install -vv \
     'restic/restic' \
     'restic_{version}_linux_amd64.bz2' \
     --extract 'restic_{version}_linux_amd64' \
